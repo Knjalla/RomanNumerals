d245085291b7b878e9566589f30c40f32025d3bd
Miikka <miklpr@outlook.com>
2017-10-11 22:37:43 +0300

    Assignment Finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,92 @@
+import java.util.*;
+import java.util.TreeMap;
 
 public class RomanNumerals {
+	public final int[] araNumbers = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+	public final String[] romNumbers = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		/*InputValidator iValidate = new InputValidator();
+		InputSlicer iSlice = new InputSlicer();
 		
+		boolean stringValid = iValidate.inputValidatorRomNum(romanNum);
+		if (stringValid == true) {
+			return 0;
+		} else {
+			}*/
+			for (int i = 0; i < romNumbers.length; i++) {
+				if (romanNum.startsWith(romNumbers[i])) {
+						return araNumbers[i] + convertToInteger(romanNum.replaceFirst(romNumbers[i], ""));
+					}
+				}return 0;
+	}
+}
+
+class InputValidator {
+	public static boolean inputValidatorRomNum(String romanNum) {
+		InputSlicer iSlice = new InputSlicer();
+		int romNumLength = InputSlicer.stringLength(romanNum);
+		char[] slicedRomNum = new char[romNumLength];
+		slicedRomNum = InputSlicer.sliceString(romanNum);
+		boolean invalidRomNum = false;
+		
+		for (int i = 0; i < romNumLength; i++) {
+			if ('I' == slicedRomNum[i]) {
+				if ('I' == slicedRomNum[i++]) {
+					if ('I' == slicedRomNum[i++]) {
+						if ('I' == slicedRomNum[i++]) {
+							invalidRomNum = true;
+						}
+					}
+				}
+			}
+		}
+		for (int i = 0; i < romNumLength; i++) {
+			if ('X' == slicedRomNum[i]) {
+				if ('X' == slicedRomNum[i++]) {
+					if ('X' == slicedRomNum[i++]) {
+						if ('X' == slicedRomNum[i++]) {
+							invalidRomNum = true;
+						}
+					}
+				}
+			}
+		}
+		for (int i = 0; i < romNumLength; i++) {
+			if ('C' == slicedRomNum[i]) {
+				if ('C' == slicedRomNum[i++]) {
+					if ('C' == slicedRomNum[i++]) {
+						if ('C' == slicedRomNum[i++]) {
+							invalidRomNum = true;
+						}
+					}
+				}
+			}
+		}
+		for (int i = 0; i < romNumLength; i++) {
+			if ('M' == slicedRomNum[i]) {
+				if ('M' == slicedRomNum[i++]) {
+					if ('M' == slicedRomNum[i++]) {
+						if ('M' == slicedRomNum[i++]) {
+							invalidRomNum = true;
+						}
+					}
+				}
+			}
+		}
+		
+		return invalidRomNum;
+	}
+}
+
+class InputSlicer {
+	public static int stringLength(String romanNum) {
+		int romLength = romanNum.length();
+		return romLength;
+	}
+	
+	public static char[] sliceString(String romanNum) {
+		
+		char[] romChar = romanNum.toCharArray();
+		return romChar;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -8,5 +8,84 @@ public class TestRomanNumerals {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testEmptyInput() {
+		RomanNumerals roNum = new RomanNumerals();
+		
+		assertEquals(0, roNum.convertToInteger(""));
+	}
+	
+	@Test
+	public void testStringLengthCalculator() {
+		InputSlicer iSlice = new InputSlicer();
+		
+		assertEquals(5, iSlice.stringLength("MMXIV"));
+	}
+	
+	@Test
+	public void testInputSlicerSliceValidString() {
+		InputSlicer iSlice = new InputSlicer();
+		
+		char[] eResult ={ 'M', 'M', 'X', 'I', 'V' };
+		
+		assertArrayEquals(eResult, iSlice.sliceString("MMXIV"));
+	}
+	
+	@Test
+	public void testInputValidatorIRepeated4TimesInRow() {
+		InputValidator iValidator = new InputValidator();
+		
+		assertEquals(true, iValidator.inputValidatorRomNum("MIIII"));
+	}
+	
+	@Test
+	public void testInputValidatorXRepeated4TimesInRow() {
+		InputValidator iValidator = new InputValidator();
+		
+		assertEquals(true, iValidator.inputValidatorRomNum("MXXXX"));
+	}
+	
+	@Test
+	public void testInputValidatorCRepeated4TimesInRow() {
+		InputValidator iValidator = new InputValidator();
+		
+		assertEquals(true, iValidator.inputValidatorRomNum("MCCCC"));
+	}
+	
+	@Test
+	public void testInputValidatorMRepeated4TimesInRow() {
+		InputValidator iValidator = new InputValidator();
+		
+		assertEquals(true, iValidator.inputValidatorRomNum("MMMM"));
+	}
+	
+	@Test
+	public void testConverterWithInvalidRomNum() {
+		RomanNumerals romNum = new RomanNumerals();
+		
+		assertEquals(0, romNum.convertToInteger("IIII"));
+	}
+	
+	@Test
+	public void testConverterWithSingleNumbers() {
+		RomanNumerals romNum = new RomanNumerals();
+		
+		assertEquals(2, romNum.convertToInteger("II"));
+	}
+	
+	@Test
+	public void testConverterWithFourNumbers() {
+		RomanNumerals romNum = new RomanNumerals();
+		
+		assertEquals(2014, romNum.convertToInteger("MMXIV"));
+	}
+	
+	@Test
+	public void testConverterWithNineNumbers() {
+		RomanNumerals romNum = new RomanNumerals();
+		
+		assertEquals(1984, romNum.convertToInteger("MCMLXXXIV"));
+	}
 
 }
